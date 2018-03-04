18e4132fa84aeab9cb9c28de306abd72b166bfc5
arne <arne@>
2018-03-04 15:48:30 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#!env python
+
+import argparse
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument(argv)
+
+
+
+if __name__=="__main__":
+    import sys
+    main(sys.argv)
+else:
+    argv=""
+    main(argv.split(" "))

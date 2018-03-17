41c753add3e4117965ee47b700caff8b9be19b7c
arne <arne@>
2018-03-17 17:25:56 +0100

    commit

--- hue.py
+++ hue.py
@@ -1,10 +1,89 @@
-import requests
+import requests, time, os, re
+
+def printError(resp):
+    print("Error: " + resp[0]["error"]["description"])
 
 class Hue:
     storage=None
 
     def __init__(self,storage):
         self.storage = storage
+
+        if not storage.get("hostname"):
+            raise RuntimeError("No hostname configured!")
+        
+        if not storage.get("username"):
+            self.auth()
     
     def sethost(self,host):
         self.storage["host"] = host
+    
+    def auth(self):
+        url = "/api"
+
+        success = False
+        while not success:
+            resp = self.rawpost(url,{"devicetype":"python_huecontroller#mba"})
+            if "error" in resp[0].keys():
+                printError(resp)
+                time.sleep(1)
+                
+            elif "success" in resp[0].keys():
+                self.storage.set("username",resp[0]["success"]["username"])
+                print("Successfully authorized")
+                success=True
+
+    
+    def getLights(self):
+        return self.get("/lights")
+    
+    def getGroups(self):
+        return self.get("/groups")
+
+    def getRules(self):
+        return self.get("/rules")
+    
+    # callback gets called with list of tuples: lightid, changed attribute, oldvalue, newvalue
+    def watch(self, callback):
+        lastState = self.getLights()
+
+        while True:
+            changes = list()
+            time.sleep(2)
+
+            currentState = self.getLights()
+
+            for lightid,lightstate in currentState.items():
+                for attribute, newvalue in lightstate.items():
+                    oldvalue = lastState[lightid][attribute]
+                    if oldvalue!=newvalue:
+                        changes.append( (lightid,attribute,oldvalue,newvalue) )
+            
+            if len(changes)>0:
+                callback(changes)
+                lastState = currentState
+
+    
+    def get(self, url):
+        assert(url[0]=="/")
+        if not self.storage.get("username"):
+            self.auth()
+
+        r = requests.get("http://" + self.storage["hostname"] + "/api/" + self.storage.get("username") + url)
+        assert(r.status_code==200)
+        return r.json()
+
+    def post(self, url, body):
+        assert(url[0]=="/")
+        if not self.storage.get("username"):
+            self.auth()
+
+        r = requests.post("http://" + self.storage["hostname"] + "/api/" + self.storage.get("username") + url, json=body)
+        assert(r.status_code==200)
+        return r.json()
+    
+    def rawpost(self, url, body):
+        assert(url[0]=="/")
+        r = requests.post("http://" + self.storage["hostname"] + url, json=body)
+        assert(r.status_code==200)
+        return r.json()
--- main.py
+++ main.py
@@ -3,18 +3,39 @@
 import argparse
 import hue
 from json_storage import JsonStorage
+from pprint import pprint
+
+
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument("-s","--set", nargs=2, type=str, help="--set KEY VALUE")
+    parser.add_argument("-l","--lights", action="store_true", dest="showlights", help="Show lights")
+    parser.add_argument("-g","--groups", action="store_true", dest="showgroups", help="Show groups")
+    parser.add_argument("-r","--rules", action="store_true", dest="showrules", help="Show rules")
+    parser.add_argument("-w","--watch", action="store_true", dest="watch", help="Watch for changes")
     options = parser.parse_args(argv)
     print(options)
 
     storage = JsonStorage("config.json")
     
+    
+    try:
+        h = hue.Hue(storage)
+        if options.showlights: 
+            pprint(h.getLights())
+        
+        if options.watch:
+            h.watch(pprint)
+        
+        if options.showgroups:
+            pprint(h.getGroups())
 
-    if options.set:
-        storage.set(options.set[0],options.set[1])
+        if options.showrules:
+            pprint(h.getRules())
+
+    except Exception as e:
+        print(e)
     
 
 
